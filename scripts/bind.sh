6f555c7672824d266f051d2b53d9273d7c461057
Ryan Davis <ryandavis.au@gmail.com>
2020-12-10 10:33:07 +1000

    feat: add scripts

---
+++ scripts/bind.sh
@@ -0,0 +1,2 @@
+sharpie bind --output=../src/TORoundedButtonBinding/TORoundedButtonBinding --namespace=TORoundedButton --sdk=iphoneos14.2 -scope ../src/TORoundedButton/TORoundedButton ../src/TORoundedButton/TORoundedButton/*.h
+
